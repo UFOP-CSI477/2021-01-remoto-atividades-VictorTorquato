b5c76edd4660590a21cb8c5e841d14d1e8b0e6ba
Victor Barboza <victortorquatob@gmail.com>
2021-12-17 20:33:31 -0300

    Atividade Prática 2 - Projeto Laravel e Migrations criados

---
+++ Atividades/atividade-pratica-02/Protocol/database/migrations/2021_12_17_231817_create_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('subject_id');
+            $table->string('person', 255);
+            $table->string('description', 255);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
